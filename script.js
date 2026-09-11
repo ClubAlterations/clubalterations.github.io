e29d9ecd25d869b8f087794870d353245cbbcdbd
Richard Myers <richard.d.myers@proton.me>
2026-09-11 11:41:15 -0500

    revert: restore original mobile actions navigation

--- script.js
+++ script.js
@@ -51,7 +51,6 @@ function applySiteData() {
 
 applySiteData();
 
-
 const nearbyMeetingLinks = document.querySelector("#nearby-meeting-links");
 if (nearbyMeetingLinks && Array.isArray(data.resources?.otherMeetings)) {
   nearbyMeetingLinks.innerHTML = data.resources.otherMeetings
@@ -292,15 +291,5 @@ if (flyerDialog && flyerDialogImage && flyerDialogLink) {
     flyerDialogImage.removeAttribute("src");
     flyerDialogImage.alt = "";
     flyerDialogLink.removeAttribute("href");
-  });
-}
-
-// Close the native mobile navigation after a menu item is selected.
-const mobileNav = document.querySelector('.mobile-nav');
-if (mobileNav) {
-  mobileNav.querySelectorAll('a[href]').forEach(link => {
-    link.addEventListener('click', () => {
-      mobileNav.removeAttribute('open');
-    });
   });
 }
